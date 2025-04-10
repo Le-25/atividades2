d5e99f36ed7c3ad58f4bce1997fdee750461bb3a
Le-25 <leticia.b.freitas@aluno.senai.br>
2025-04-10 11:31:58 -0300

    Update atividade5.js

---
+++ atividade5.js
@@ -0,0 +1,11 @@
+
+// 5. Qual o valor da variável resultado após executar esse código?
+
+// function dobro(numero) {
+//   return numero * 2;
+// }
+
+// resposta
+// Essa função recebe um número e retorna o dobro dele.
+// dobro(5) → retorna 5 * 2 → 10
+// const resultado = 10;
