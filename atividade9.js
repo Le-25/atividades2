0f4dbb418d755fadb59fe3aac68850cd8e1520cf
Le-25 <leticia.b.freitas@aluno.senai.br>
2025-04-11 08:09:40 -0300

    Update atividade9.js

---
+++ atividade9.js
@@ -0,0 +1,18 @@
+// 9. A função abaixo está com problema. Ela deveria retornar o valor 10, mas não retorna nada. Corrija-a:
+
+// function calcularSoma(x, y) {
+//   x + y;
+// }
+
+
+// resposta:
+// Problema:
+// A função está apenas somando x + y, mas não está retornando o resultado.
+
+// Correção:
+
+function calcularSoma(x, y) {
+  return x + y;
+} 
+
+console.log(calcularSoma(5,5));
