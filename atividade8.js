3b0da1ce7065b619edbbb1a08441b35cfe3f420e
Le-25 <leticia.b.freitas@aluno.senai.br>
2025-04-11 08:08:06 -0300

    Update atividade8.js

--- atividade8.js
+++ atividade8.js
@@ -1,8 +1,15 @@
 // 8. O que acontece se chamarmos uma função antes de declará-la? Teste e explique o seguinte código:
 
+
 dizerOi();
 
 function dizerOi() {
   console.log("Oi!");
 }
 
+// O que acontece? 
+// Ele mostra "Oi!" no console.
+
+// Por quê?
+// Porque o JavaScript entende a função antes de chegar nela.
+// Então, mesmo chamando antes, funciona.
